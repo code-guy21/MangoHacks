dc0a91bef2d83f91982a86abace9f89ffb3db9b1
code-guy21 <San-Javier@Alexiss-MacBook-Pro.local>
2017-02-25 13:46:11 -0500

    new changes

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -8,16 +8,23 @@
 
 import UIKit
 import GoogleMaps
+import Toucan
 
 class ViewController: UIViewController {
     
     override func loadView() {
         
         
-        let image = #imageLiteral(resourceName: "Miguel")
+        let mig = #imageLiteral(resourceName: "miguel")
         
-        let imageview = UIImageView(image: image)
-        imageview.layer.borderColor = UIColor.blue.cgColor
+        let newmig = Toucan(image: mig).resize(CGSize(width: 70, height: 70)).maskWithEllipse(borderWidth: 5, borderColor: UIColor.red).image
+        
+        let bran = #imageLiteral(resourceName: "brandon")
+        
+        let newbran = Toucan(image: bran).resize(CGSize(width: 70, height: 70)).maskWithEllipse(borderWidth: 5, borderColor: UIColor.green).image
+        
+        let gia = #imageLiteral(resourceName: "giani")
+        let newgia = Toucan(image: gia).resize(CGSize(width: 70, height: 70)).maskWithEllipse(borderWidth: 5, borderColor: UIColor.yellow).image
         
         // Create a GMSCameraPosition that tells the map to display the
         // coordinate -33.86,151.20 at zoom level 6.
@@ -26,12 +33,28 @@ class ViewController: UIViewController {
         view = mapView
         
         // Creates a marker in the center of the map.
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: 25.7565756, longitude: -80.3739487)
-        marker.title = "Active"
-        marker.snippet = "Throwing a party at my house"
-        marker.icon = image
-        marker.map = mapView
+        let marker1 = GMSMarker()
+        marker1.position = CLLocationCoordinate2D(latitude: 25.7565756, longitude: -80.3739487)
+        marker1.title = "Active"
+        marker1.snippet = "Throwing a party at my house"
+        marker1.icon = newmig
+        marker1.map = mapView
+        
+        // Creates a marker in the center of the map.
+        let marker2 = GMSMarker()
+        marker2.position = CLLocationCoordinate2D(latitude: 40, longitude: -70)
+        marker2.title = "Active"
+        marker2.snippet = "Having dinner with my family"
+        marker2.icon = newbran
+        marker2.map = mapView
+        
+        // Creates a marker in the center of the map.
+        let marker3 = GMSMarker()
+        marker3.position = CLLocationCoordinate2D(latitude: 30, longitude: -60)
+        marker3.title = "All the way up"
+        marker3.snippet = "Vacationing"
+        marker3.icon = newgia
+        marker3.map = mapView
     
         
     }
